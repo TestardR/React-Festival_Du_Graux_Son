76670073120e7fbfa9720b720c4061f589cffca7
unknown <rmtestard@gmail.com>
2019-06-08 15:35:13 +0200

    home logo done

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,22 @@ import BaseLayout from './layouts/BaseLayout';
 const Home = () => {
   return (
     <BaseLayout className="homeBackground">
-      Home
+      <div className="homeLogo">
+        <div className="title">
+          <span>{`FesTiVAL
+      du
+      GRAUX soN`}</span>
+        </div>
+        <div className="edition info">
+          <span>2ème édition</span>
+        </div>
+        <div className="info">
+          <span>
+            {`11-12-13 Octobre 2019
+      Tournai, Belgique`}
+          </span>
+        </div>
+      </div>
       <Sponsor />
     </BaseLayout>
   );
