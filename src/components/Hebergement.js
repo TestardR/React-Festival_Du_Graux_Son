73a0add71af7fb18f72d6a66905b0a4ddbd4417e
unknown <rmtestard@gmail.com>
2019-06-08 14:23:14 +0200

    common background for the whole app

---
+++ src/components/Hebergement.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hebergement = () => {
+  return <div>Heb</div>;
+};
+
+export default Hebergement;
---
+++ src/components/Restauration.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Restauration = () => {
+  return <div>Resto</div>;
+};
+
+export default Restauration;
