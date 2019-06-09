4ce7be978f81c5256e5fd44533280bca5960b8ce
unknown <rmtestard@gmail.com>
2019-06-09 14:53:39 +0200

    linup images on hover

--- src/components/LineUp.js
+++ src/components/LineUp.js
@@ -1,104 +1,237 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BaseLayout from './layouts/BaseLayout';
 import { Row, Col } from 'reactstrap';
 import { Image } from 'react-bootstrap';
 import LineupBase from '../static/images/lineupBase.jpg';
+import LineupSky from '../static/images/lineupSky.jpg';
+import LineupHotdog from '../static/images/lineupHotdog.jpg';
+import LineupWar from '../static/images/lineupWar.jpg';
+import LineupHippy from '../static/images/lineupHippy.jpg';
 
-const LineUp = () => {
-  return (
-    <BaseLayout className="lineupBackground">
-      <div className="lineupWrapper">
-        <Row className="justify-content-around">
-          <Col xs="12" md="8" lg="8">
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                Banzai
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                Kamaka
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                Roloko
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                ullamco
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                fugiat
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                cupidatat
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                Veniam
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                incididunt
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                adipisicing
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                magna
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                commodo
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                pariatur
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                cillum
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                esse
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                sint
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                dolore
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                deserunt
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                Consequat
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                amet
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                irure
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                mollit
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                aute
-              </Col>
-              <Col className="lineupItem" xs="auto" md="auto" lg="auto">
-                fugiat
-              </Col>
-            </Row>
-          </Col>
-          <Col xs="0" md="4" lg="4">
-            <Image src={`${LineupBase}`} roundedCircle />
-          </Col>
-        </Row>
-      </div>
-    </BaseLayout>
-  );
-};
+class LineUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: LineupBase
+    };
+  }
+
+  handleMouseOver(src) {
+    this.setState({
+      imgSrc: src
+    });
+  }
+
+  handleMouseOut() {
+    this.setState({
+      imgSrc: LineupBase
+    });
+  }
+
+  render() {
+    return (
+      <BaseLayout className="lineupBackground">
+        <div className="lineupWrapper">
+          <Row className="justify-content-around">
+            <Col xs="12" md="8" lg="7">
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupSky)}
+                  onMouseOut={() => this.handleMouseOut()}
+                  src={this.state.imgSrc}
+                >
+                  Banzai
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHotdog)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  Kamaka
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupWar)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  Roloko
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHippy)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  ullamco
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupWar)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  fugiat
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHotdog)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  cupidatat
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupWar)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  Veniam
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHippy)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  incididunt
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHotdog)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  adipisicing
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupSky)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  magna
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupSky)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  commodo
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHippy)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  pariatur
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupWar)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  cillum
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHippy)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  esse
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHotdog)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  deserunt
+                </Col>
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupSky)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  Consequat
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Col
+                  className="lineupItem"
+                  xs="auto"
+                  md="auto"
+                  lg="auto"
+                  onMouseOver={() => this.handleMouseOver(LineupHippy)}
+                  onMouseOut={() => this.handleMouseOut()}
+                >
+                  deserunt
+                </Col>
+              </Row>
+            </Col>
+            <Col xs="0" md="4" lg="5">
+              <Image
+                src={this.state.imgSrc}
+                roundedCircle
+                className="lineupImage"
+              />
+            </Col>
+          </Row>
+        </div>
+      </BaseLayout>
+    );
+  }
+}
 
 export default LineUp;
